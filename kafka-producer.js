c83589d78e02e16835f0eec06aa07c96ea6c3675
iamshaveenvf <shaveenfernando1@gmail.com>
2023-03-16 23:00:19 +0530

    initial commit

---
+++ kafka-producer.js
@@ -0,0 +1,23 @@
+const coap = require('coap')
+const { KafkaClient, Producer } = require('kafka-node')
+
+// Set up Kafka producer
+const kafkaClient = new KafkaClient({ kafkaHost: 'localhost:9092' })
+const producer = new Producer(kafkaClient)
+
+// Set up CoAP server
+const coapServer = coap.createServer(function (req, res) {
+  // Handle incoming CoAP requests and send data to Kafka producer
+  const data = req.payload.toString()
+  producer.send([{ topic: 'my_topic', messages: data }], function (err, result) {
+    if (err) console.error(err)
+    console.log('Data sent to Kafka')
+  })
+
+  res.end('OK')
+})
+
+// Start CoAP server
+coapServer.listen(5683, '127.0.0.1', function () {
+  console.log('CoAP server listening')
+})
